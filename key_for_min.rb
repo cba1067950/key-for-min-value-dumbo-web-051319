e68e89612e43c5a2145f0f649f5f2a56db54037a
Joe Jamsky <cba1067950@yahoo.com>
2019-05-01 05:06:17 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,16 +6,4 @@ def key_for_min_value(name_hash)
   minArray = []
   
   minArray = name_hash.collect { |key, value| value }
-  
-  lowest = 0
-  i = 0
-  while i < minArray.length
-    if minArray[i] < minArray[i+1]
-      lowest = minArray[i]
-    else
-      lowest = minArray[i+1]
-    end
-    i += 1
-  end
-  lowest
 end
