5c962adcefe9ec125279b7a319bbaf9611dc93e4
Joe Jamsky <cba1067950@yahoo.com>
2019-05-01 05:09:59 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,6 +7,7 @@ def key_for_min_value(name_hash)
   minArray = name_hash.collect { |key, value| value }
   
   minimum = minArray[0]
+  puts minimum
   minArray.each do |num|
     if num < minimum
       minimum = num
