aed11294bc2a0f79de15a20c02010e1b596f7ed6
Joe Jamsky <cba1067950@yahoo.com>
2019-05-01 04:53:30 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,10 +2,11 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  minArray = []
   smallest = 0
   check = 0
-  name_hash.collect do |key, value|
-    
+  minArray = name_hash.collect { |key, value| value }
+  minArray.each do |value|
+    puts value
   end
-    
 end
