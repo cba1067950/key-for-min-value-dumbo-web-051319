99b64277e33976e523fed5b30723962d74127397
Joe Jamsky <cba1067950@yahoo.com>
2019-05-01 05:05:18 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,18 +1,20 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
-
+#[10, 50, 17]
+#[500, 2, 1]
 def key_for_min_value(name_hash)
   minArray = []
-  smallest = 0
-  largest = 0
-  check = 0
+  
   minArray = name_hash.collect { |key, value| value }
-  minArray.each do |value|
-    if value > smallest
-      largest = value 
+  
+  lowest = 0
+  i = 0
+  while i < minArray.length
+    if minArray[i] < minArray[i+1]
+      lowest = minArray[i]
     else
-      smallest = value
+      lowest = minArray[i+1]
     end
   end
-  smallest
+  lowest
 end
